c7646f3c0bc4ceb2eadd996b30541b0d92fb2663
ohminyoung <minyoung_stat@dgu.ac.kr>
2022-12-02 00:30:55 +0900

    ButtonStyle, Padding

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,40 +12,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤 숫자 생성기'),
-                IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
-              ],
-            ),
-            Expanded(
-              child: SizedBox(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '랜덤 숫자 생성기',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                      color: RED_COLOR,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('123'),
+                      Text('456'),
+                      Text('789'),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
                 width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('123'),
-                    Text('456'),
-                    Text('789'),
-                  ],
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+                  onPressed: () {},
+                  child: Text(
+                    '생성하기',
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  '생성하기',
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
